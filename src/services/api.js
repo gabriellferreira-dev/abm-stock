85572bd03ab4b342f3b11308bc5ff0759ebdcd64
Gabriel Ferreira <glucas709@gmail.com>
2021-11-02 15:05:57 -0300

    feat: crud functions and test

---
+++ src/services/api.js
@@ -0,0 +1,39 @@
+const URL = process.env.REACT_APP_CRUDCRUD_URL;
+
+export const create = async (endpoint, data) => {
+  const response = await fetch(`${URL}/${endpoint}`, {
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+  const created = await response.json();
+  return created;
+};
+
+export const get = async (endpoint) => {
+  const response = await fetch(`${URL}/${endpoint}`);
+  const data = await response.json();
+  return data;
+};
+
+export const getById = async (endpoint, id) => {
+  const response = await fetch(`${URL}/${endpoint}/${id}`);
+  const data = await response.json();
+  return data;
+};
+
+export const edit = async (endpoint, id, data) => {
+  const response = await fetch(`${URL}/${endpoint}/${id}`, {
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    method: 'PUT',
+    body: JSON.stringify(data),
+  });
+  const updated = await response.json();
+  return updated;
+};
+
+export const remove = async (endpoint, id) => {
+  await fetch(`${URL}/${endpoint}/${id}`, {
+    method: 'DELETE',
+  });
+};
---
+++ src/utils/products.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    _id: 'asd623',
+    name: 'Abóbora',
+    quantity: 50,
+    price: '10.99',
+    active: true,
+  },
+  {
+    _id: 'asd624',
+    name: 'Mamão',
+    quantity: 50,
+    price: '1.99',
+    active: true,
+  },
+  {
+    _id: 'asd625',
+    name: 'Bis',
+    quantity: 50,
+    price: '0.99',
+    active: true,
+  },
+];
